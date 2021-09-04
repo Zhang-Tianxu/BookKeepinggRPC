422b62137d8c8e591a330d7318898cc74585bc89
tianxuzhang <tianxuzhang@tencent.com>
2021-09-04 12:35:22 +0800

    init

--- protoCompile.sh
+++ protoCompile.sh
@@ -3,6 +3,7 @@ mkdir ./out
 mkdir ./out/cpp
 mkdir ./out/objc
 mkdir ./out/go
+mkdir ./out/objcgrpc
 
-protoc -I=./source --cpp_out=./out/cpp --go_out=./out/go --objc_out=./out/objc ./**/*.proto
+protoc --plugin=/usr/local/bin/grpc_objective_c_plugin -I=./source --cpp_out=./out/cpp --go_out=./out/go --objc_out=./out/objc --objcgrpc_out=./out/objcgrpc ./**/*.proto
 
